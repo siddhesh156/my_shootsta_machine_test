11ce3d553991d8de4fc453559cc324b651ac532f
Siddhesh <siddhesh.chavan@techno-soft.in>
2022-02-25 15:34:37 +0530

    try/catch removed

--- src/container/Dictionary/index.js
+++ src/container/Dictionary/index.js
@@ -56,95 +56,88 @@ const Dictionary = () => {
     setWord(event.target.value);
   };
 
-  try {
-    return (
-      <>
-        <div className="main-container">
-          <h1>Dictionary</h1>
-          <InputGroup className="mb-3" hasValidation>
-            <FormControl
-              placeholder="Search"
-              aria-label="Search"
-              aria-describedby="basic-addon1"
-              value={word}
-              required
-              isInvalid={checkValid}
-              onChange={handleChange}
-              onKeyDown={handleKeyDown}
-            />
-            <FormControl.Feedback type="invalid" tooltip>
-              Please enter a word.
-            </FormControl.Feedback>
-          </InputGroup>
+  return (
+    <div className="main-container">
+      <h1>Dictionary</h1>
+      <InputGroup className="mb-3" hasValidation>
+        <FormControl
+          placeholder="Search"
+          aria-label="Search"
+          aria-describedby="basic-addon1"
+          value={word}
+          required
+          isInvalid={checkValid}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+        />
+        <FormControl.Feedback type="invalid" tooltip>
+          Please enter a word.
+        </FormControl.Feedback>
+      </InputGroup>
 
-          <Button
-            variant="dark"
-            style={{ alignSelf: "center", width: "70%" }}
-            onClick={onHandleSearch}
-          >
-            Submit
-          </Button>
+      <Button
+        variant="dark"
+        style={{ alignSelf: "center", width: "70%" }}
+        onClick={onHandleSearch}
+      >
+        Submit
+      </Button>
 
-          {isLoading ? (
-            <div className="mt-5 d-f as-c">
-              <Loading />
-            </div>
-          ) : (
-            <>
-              {searchData.length > 0 && (
-                <div className="mt-5">
-                  <Card style={{ background: "#7c795d" }}>
-                    <Card.Body
-                      style={{
-                        color: "rgb(247, 154, 12)",
-                        fontFamily: "Trocchi, serif",
-                      }}
-                    >
-                      <Card.Title style={{ textAlign: "center" }}>
-                        Definitions
-                      </Card.Title>
-                      <ul>
-                        {searchData.map((data, index) => {
-                          let checkExample = data?.example !== undefined;
-                          return (
-                            <li key={index}>
-                              {data.definition}
-                              {checkExample
-                                ? " For example " + data?.example
-                                : null}
-                            </li>
-                          );
-                        })}
-                      </ul>
-                    </Card.Body>
-                  </Card>
-                </div>
-              )}
-
-              {errorMessage !== "" ? (
-                <div className="mt-5">
-                  <Card style={{ background: "#7c795d" }}>
-                    <Card.Body
-                      style={{
-                        color: "rgb(247, 154, 12)",
-                        fontFamily: "Trocchi, serif",
-                      }}
-                    >
-                      <Card.Title style={{ textAlign: "center" }}>
-                        {errorMessage}
-                      </Card.Title>
-                    </Card.Body>
-                  </Card>
-                </div>
-              ) : null}
-            </>
-          )}
+      {isLoading ? (
+        <div className="mt-5 d-f as-c">
+          <Loading />
         </div>
-      </>
-    );
-  } catch (error) {
-    console.log("Dictionary Error ", error);
-    return null;
-  }
+      ) : (
+        <>
+          {searchData.length > 0 && (
+            <div className="mt-5">
+              <Card style={{ background: "#7c795d" }}>
+                <Card.Body
+                  style={{
+                    color: "rgb(247, 154, 12)",
+                    fontFamily: "Trocchi, serif",
+                  }}
+                >
+                  <Card.Title style={{ textAlign: "center" }}>
+                    Definitions
+                  </Card.Title>
+                  <ul>
+                    {searchData.map((data, index) => {
+                      let checkExample = data?.example !== undefined;
+                      return (
+                        <li key={index}>
+                          {data.definition}
+                          {checkExample
+                            ? " For example " + data?.example
+                            : null}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </Card.Body>
+              </Card>
+            </div>
+          )}
+
+          {errorMessage !== "" ? (
+            <div className="mt-5">
+              <Card style={{ background: "#7c795d" }}>
+                <Card.Body
+                  style={{
+                    color: "rgb(247, 154, 12)",
+                    fontFamily: "Trocchi, serif",
+                  }}
+                >
+                  <Card.Title style={{ textAlign: "center" }}>
+                    {errorMessage}
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
+          ) : null}
+        </>
+      )}
+    </div>
+  );
 };
 export default Dictionary;
